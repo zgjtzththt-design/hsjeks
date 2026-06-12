a52e15cc078313da3e7d4a87368fdf733d488c88
zgjtzththt-design <zgjtzththt@gmail.com>
2026-06-12 17:12:13 +0100

    build: remove redundant APK copy tasks
    Remove unnecessary build tasks used for manual APK deployment to simplify the root build script.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,21 +7,3 @@ plugins {
   alias(libs.plugins.roborazzi) apply false
   alias(libs.plugins.secrets) apply false
 }
-
-tasks.register<Copy>("copyDebugApk") {
-    dependsOn(":app:assembleDebug")
-    from("app/build/outputs/apk/debug/app-debug.apk")
-    into(".build-outputs")
-}
-
-tasks.register<Copy>("copyDebugApkToArabicDownload") {
-    dependsOn(":app:assembleDebug")
-    from("app/build/outputs/apk/debug/app-debug.apk")
-    into("تنزيل ملف APK")
-}
-
-tasks.register<Copy>("copyDebugApkToDownload") {
-    dependsOn(":app:assembleDebug")
-    from("app/build/outputs/apk/debug/app-debug.apk")
-    into("APK_DOWNLOAD")
-}
